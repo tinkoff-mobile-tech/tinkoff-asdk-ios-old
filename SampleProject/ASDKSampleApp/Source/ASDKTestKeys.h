fb8a512238bd95c781f6a04bb805f100ca5089a3
Budnikov Vyacheslav <v.budnikov@tinkoff.ru>
2020-04-09 11:25:28 +0300

    update TestTerminal credential

--- SampleProject/ASDKSampleApp/Source/ASDKTestKeys.h
+++ SampleProject/ASDKSampleApp/Source/ASDKTestKeys.h
@@ -21,7 +21,7 @@ replace this constants:
 #define kASDKTestTerminalKey2 @"mTerminal"
 #define kASDKTestTerminalKey3 @"sdkNon3DS"
 
-#define kASDKTestPassword @"12345678"
+#define kASDKTestPassword @"5l9v23g7hlhqchyb"
 #define kASDKTestPublicKey @"MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAqBiorLS9OrFPezixO5lSsF+HiZPFQWDO7x8gBJp4m86Wwz7ePNE8ZV4sUAZBqphdqSpXkybM4CJwxdj5R5q9+RHsb1dbMjThTXniwPpJdw4WKqG5/cLDrPGJY9NnPifBhA/MthASzoB+60+jCwkFmf8xEE9rZdoJUc2p9FL4wxKQPOuxCqL2iWOxAO8pxJBAxFojioVu422RWaQvoOMuZzhqUEpxA9T62lN8t3jj9QfHXaL4Ht8kRaa2JlaURtPJB5iBM+4pBDnqObNS5NFcXOxloZX4+M8zXaFh70jqWfiCzjyhaFg3rTPE2ClseOdS7DLwfB2kNP3K0GuPuLzsMwIDAQAB"
 
 #endif /* ASDKTestKeys_h */
